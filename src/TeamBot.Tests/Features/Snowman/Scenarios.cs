b37ce3a852689106d6cefbfc27e04ae4aacf4f50
Brendan Graham <brendan@graham.im>
2014-07-09 23:16:10 +1000

    Added snowman feature

---
+++ src/TeamBot.Tests/Features/Snowman/Scenarios.cs
@@ -0,0 +1,21 @@
+﻿using NSubstitute;
+using TeamBot.Features.Snowman;
+using TeamBot.Infrastructure.Slack.Models;
+using TeamBot.Tests.Specifications;
+
+namespace TeamBot.Tests.Features.Snowman
+{
+    public class Scenarios : HandlerScenarioBase<SnowmanHandler>
+    {
+        public override string Request
+        {
+            get { return "do you want to build a snowman?"; }
+        }
+
+        [Then]
+        public override void ShouldRespondCorrectly()
+        {
+            Subject.Slack.Received().SendAsync(IncomingMessage.ReplyTo(), ":snowman:");
+        }
+    }
+}
---
+++ src/TeamBot/Features/Snowman/SnowmanHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TeamBot.Infrastructure.Messages;
+using TeamBot.Infrastructure.Slack;
+using TeamBot.Infrastructure.Slack.Models;
+
+namespace TeamBot.Features.Snowman
+{
+    public class SnowmanHandler : SlackMessageHandler
+    {
+        public SnowmanHandler(ISlackClient slack) : base(slack)
+        {
+        }
+
+        public override string Help()
+        {
+            return "do you want to build a snowman?";
+        }
+
+        public override async Task Handle(IncomingMessage incomingMessage)
+        {
+            if (incomingMessage == null)
+                throw new ArgumentNullException("incomingMessage");
+
+            const string pattern = @"do you want to build a snowman\?*$";
+            const string snowman = ":snowman:";
+
+            if (Regex.IsMatch(incomingMessage.Text, pattern))
+                await Slack.SendAsync(incomingMessage.ReplyTo(), snowman);
+        }
+    }
+}
